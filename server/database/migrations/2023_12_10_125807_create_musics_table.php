660f6cf8202c868e6eef43268306d02f5e866a2c
Mateus Soares <mateussoares114@gmail.com>
2023-12-16 08:32:08 -0300

    Update migration Musics

--- server/database/migrations/2023_12_10_125807_create_musics_table.php
+++ server/database/migrations/2023_12_10_125807_create_musics_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('music', function (Blueprint $table) {
+        Schema::create('musics', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('artist', 128);
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('music');
+        Schema::dropIfExists('musics');
     }
 };
