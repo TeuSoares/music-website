19f157469609dec59ccb711537534b66354e515f
Mateus Soares <mateussoares114@gmail.com>
2023-12-17 10:04:20 -0300

    Update UserTest

--- server/tests/Feature/UserTest.php
+++ server/tests/Feature/UserTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use Domain\User\Models\User;
 use Domain\User\Requests\UserRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -33,23 +32,23 @@ class UserTest extends TestCase
         $this->assertDatabaseHas('users', ['email' => $data['email']]);
     }
 
-    public function test_if_creating_new_user_fail_throw_exception(): void
+    public function test_validation_rules_at_create_new_user(): void
+    {
+        $request = new UserRequest();
+        $rules = $request->rules();
+        $validator = Validator::make([], $rules);
+        $fails = $validator->fails();
+
+        $this->assertEquals(true, $fails);
+    }
+
+    public function test_if_email_already_exist_return_error(): void
     {
         $user = User::newFactory()->create()->toArray();
 
-        $user['password'] = 12345678;
-        $user['password_confirmation'] = 12345678;
-
-        $response = $this->postJson('/api/user', $user);
-
-        $response->assertStatus(500)
-            ->assertSee('Failed to create new user. Please try again');
-    }
-
-    public function test_validation_rules_at_create_new_user(): void
-    {
         $data = [
-            'email' => "teste@teste.com",
+            'name' => 'test',
+            'email' => $user['email'],
             'password' => 12345678,
             'password_confirmation' => 12345678
         ];
