8a2ec81926101b67bc8f389224507ab769800036
Mateus Soares <mateussoares114@gmail.com>
2023-12-17 10:03:51 -0300

    Add rule to check if email already exists

--- server/app/Domain/User/Requests/UserRequest.php
+++ server/app/Domain/User/Requests/UserRequest.php
@@ -10,7 +10,7 @@ class UserRequest extends Request
     {
         return [
             'name'                  => 'required|string',
-            'email'                 => 'required|email',
+            'email'                 => 'required|email|unique:Domain\User\Models\User',
             'password'              => 'required|min:6|max:12|confirmed',
             'password_confirmation' => 'required|min:6|max:12|'
         ];
