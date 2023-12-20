92939067d0b6081fb0791cad55a11e0fa1bbf843
Mateus Soares <mateussoares114@gmail.com>
2023-12-20 06:20:49 -0300

    Update test incorrect password and create test to validation rules login, reset password

--- server/tests/Feature/AuthTest.php
+++ server/tests/Feature/AuthTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use Domain\Auth\Requests\LoginRequest;
 use Domain\User\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -42,8 +45,40 @@ class AuthTest extends TestCase
             'password' => 'invalid-password'
         ]);
 
-        $response->assertStatus(422);
+        $response->assertStatus(422)
+            ->assertExactJson([
+                'message' => 'Email or password is incorrect',
+                'errors' => [
+                    'authetication' => ['Email or password is incorrect']
+                ]
+            ]);
 
         $this->assertGuest();
+    }
+
+    public function test_validation_rules_at_attempt_login(): void
+    {
+        $request = new LoginRequest();
+        $rules = $request->rules();
+        $validator = Validator::make([], $rules);
+        $fails = $validator->fails();
+
+        $this->assertEquals(true, $fails);
+    }
+
+    public function test_if_mail_is_valid_it_send_mail_to_reset_password(): void
+    {
+        Notification::fake();
+
+        $user = User::newFactory()->create();
+
+        $response = $this->postJson('/api/forgot-password', [
+            'email' => $user->email
+        ]);
+
+        $response->assertStatus(200)
+            ->assertSee('Reset Password')
+            ->assertSee('E-Mail Address')
+            ->assertSee('Send Password Reset Link');
     }
 }
