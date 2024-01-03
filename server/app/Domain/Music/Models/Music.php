575df91f232801f2ad1782eea517b7d17c5d9c5e
TeuSoares <mateussoares114@gmail.com>
2024-01-03 17:47:09 -0300

    Create MusicFactory

--- server/app/Domain/Music/Models/Music.php
+++ server/app/Domain/Music/Models/Music.php
@@ -3,6 +3,7 @@
 namespace Domain\Music\Models;
 
 use App\Core\Model;
+use Database\Factories\MusicFactory;
 use Domain\User\Models\User;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +18,11 @@ class Music extends Model
         return Attribute::make(
             set: fn (string $value) => 'https://www.youtube.com/embed/' . $value,
         );
+    }
+
+    protected static function newFactory(): MusicFactory
+    {
+        return MusicFactory::new();
     }
 
     public function user(): BelongsTo
---
+++ server/database/factories/MusicFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Domain\Music\Models\Music;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Music\Models\Music>
+ */
+class MusicFactory extends Factory
+{
+    protected $model = Music::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'artist' => fake()->firstName(),
+            'genre' => 'Rock',
+            'name' => fake()->sentence(),
+            'link' => '8AHCfZTRGiI',
+            'thumbnail' => fake()->image()
+        ];
+    }
+}
