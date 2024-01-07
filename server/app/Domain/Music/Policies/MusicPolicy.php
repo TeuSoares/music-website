a30a97ee33e57ddd1d58cf05614f3937f32aa468
Mateus Soares <mateussoares114@gmail.com>
2024-01-07 11:13:33 -0300

    Add method to delete and update music

--- server/app/Domain/Music/Policies/MusicPolicy.php
+++ server/app/Domain/Music/Policies/MusicPolicy.php
@@ -11,4 +11,14 @@ class MusicPolicy
     {
         return $user->id === $music->user_id;
     }
+
+    public function update(User $user, Music $music): bool
+    {
+        return $user->id === $music->user_id;
+    }
+
+    public function delete(User $user, Music $music): bool
+    {
+        return $user->id === $music->user_id;
+    }
 }
