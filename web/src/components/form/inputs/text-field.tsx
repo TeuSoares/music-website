94715decd529971c7f05a1812dd892b42adf3230
Mateus Soares <mateussoares114@gmail.com>
2024-02-19 06:25:20 -0300

    feat: add text-field component

---
+++ web/src/components/form/inputs/text-field.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from 'react'
+import { useFormContext } from 'react-hook-form'
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+interface TextFieldProps {
+  name: string
+  label: string
+  type?: string
+  placeholder?: string
+  description?: string | ReactNode
+  defaultValue?: string | number
+  disabled?: boolean
+}
+
+const TextField = ({
+  name,
+  label,
+  placeholder,
+  description,
+  type = 'text',
+  defaultValue = '',
+  disabled = false,
+}: TextFieldProps) => {
+  const { control } = useFormContext()
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      defaultValue={defaultValue}
+      disabled={disabled}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input type={type} placeholder={placeholder} {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default TextField
