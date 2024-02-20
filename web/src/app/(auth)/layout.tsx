bcbb78d56dda1896f1ea431e26114afc3580f8e6
TeuSoares <mateussoares114@gmail.com>
2024-02-20 11:09:43 -0300

    refactor: add card component at the auth layout

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from 'react'
 
+import { Card } from '@/components/ui/card'
+
 export default function RootLayoutAuth({ children }: { children: ReactNode }) {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      {children}
+      <Card className="w-[450px]">{children}</Card>
     </div>
   )
 }
