c359eda808bebb04b9c8c4202b915b41ce13ce6f
TeuSoares <mateussoares114@gmail.com>
2024-02-20 14:52:18 -0300

    refactor: add breakpoint

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import { Card } from '@/components/ui/card'
 export default function RootLayoutAuth({ children }: { children: ReactNode }) {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <Card className="w-[450px]">{children}</Card>
+      <Card className="min-[450px]:w-[450px]">{children}</Card>
     </div>
   )
 }
