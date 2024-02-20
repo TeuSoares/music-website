4ee3d4a92fa4e955d7d805ff990bcef37c84c0e7
Mateus Soares <mateussoares114@gmail.com>
2024-02-20 06:26:18 -0300

    remove card component

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react'
 
-import { Card } from '@/components/ui/card'
-
 export default function RootLayoutAuth({ children }: { children: ReactNode }) {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <Card className="w-[450px]">{children}</Card>
+      {children}
     </div>
   )
 }
