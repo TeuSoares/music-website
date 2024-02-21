034ee5352ec9b5a7aa5a2c767b0ba7d703271a59
Mateus Soares <mateussoares114@gmail.com>
2024-02-21 06:21:24 -0300

    refactor: changing the body of the message, using the card component

--- web/src/app/(auth)/(pages)/email/verify/[id]/[hash]/page.tsx
+++ web/src/app/(auth)/(pages)/email/verify/[id]/[hash]/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 
+import BulletPoint from '@/components/layout/bullet-point'
 import LinkUnderline from '@/components/layout/link-underline'
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-
-import { Terminal } from 'lucide-react'
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 interface VerifyEmailProps {
   params: {
@@ -14,24 +20,45 @@ interface VerifyEmailProps {
   }
 }
 
-export default function VerifyEmail({ id, hash }: VerifyEmailProps) {
+export default function VerifyEmail({ params }: VerifyEmailProps) {
   const searchParams = useSearchParams()
 
-  const handleVerifyEmail = async () => {
-    const signed = searchParams.get('signed')
-    console.log(signed)
-  }
+  useEffect(() => {
+    const handleVerifyEmail = async () => {
+      const expires = searchParams.get('expires')
+      const signature = searchParams.get('signature')
+
+      const url = `/email/verify/${params.id}/${params.hash}?expires=${expires}&signature=${signature}`
+      console.log(url)
+    }
+
+    handleVerifyEmail()
+  }, [searchParams, params])
 
   return (
-    <Alert>
-      <Terminal className="h-4 w-4" />
-      <AlertTitle>Your email has been successfully verified</AlertTitle>
-      <AlertDescription className="flex flex-col">
-        {`Your email has been successfully verified. Now you can listen to your music without any problems`}
+    <>
+      <CardHeader>
+        <CardTitle>Successful Email Verification</CardTitle>
+        <CardDescription>
+          Your email has been successfully verified. Now you can listen to your
+          music without any problems
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <BulletPoint
+          title="Have fun with favorite songs"
+          description="Listen to your musics anywhere."
+        />
+        <BulletPoint
+          title="Practicality in everyday life"
+          description="Manage your music in one place."
+        />
+      </CardContent>
+      <CardFooter>
         <LinkUnderline href="/" className="mt-2">
-          Click here to back home
+          Click here to return to home page
         </LinkUnderline>
-      </AlertDescription>
-    </Alert>
+      </CardFooter>
+    </>
   )
 }
