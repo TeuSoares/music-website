54378055fad1df93202b1f604c16310ff2f98459
TeuSoares <mateussoares114@gmail.com>
2024-02-21 17:44:08 -0300

    refactor: using toaster component

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react'
 
 import { Card } from '@/components/ui/card'
+import { Toaster } from '@/components/ui/toaster'
 
 export default function RootLayoutAuth({ children }: { children: ReactNode }) {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <Card className="min-[450px]:w-[450px]">{children}</Card>
-    </div>
+    <>
+      <Toaster />
+      <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+        <Card className="min-[450px]:w-[450px]">{children}</Card>
+      </div>
+    </>
   )
 }
