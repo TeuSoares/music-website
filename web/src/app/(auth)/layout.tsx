318ed86c73b0223df3efdec37645fc646084584b
TeuSoares <mateussoares114@gmail.com>
2024-02-22 17:21:03 -0300

    feat: add loading component

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 
+import Loading from '@/components/layout/loading'
 import { Card } from '@/components/ui/card'
 import { Toaster } from '@/components/ui/toaster'
 
@@ -7,6 +8,7 @@ export default function RootLayoutAuth({ children }: { children: ReactNode }) {
   return (
     <>
       <Toaster />
+      <Loading />
       <div className="w-[100vw] h-[100vh] flex justify-center items-center">
         <Card className="min-[450px]:w-[450px]">{children}</Card>
       </div>
---
+++ web/src/components/layout/loading.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useAppContext } from '@/hooks'
+import { Loader2 } from 'lucide-react'
+
+const Loading = () => {
+  const { isLoading } = useAppContext()
+
+  return (
+    <>
+      {isLoading && (
+        <div className="absolute">
+          <div className="w-[100vw] h-[100vh] p-4 bg-black/30">
+            <Loader2 className="h-20 w-20 text-[#BC2627] animate-spin" />
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Loading
