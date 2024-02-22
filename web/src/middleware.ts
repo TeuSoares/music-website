f89af1ebec8ea48c5b9250b111e1da0fd5ed7c3b
TeuSoares <mateussoares114@gmail.com>
2024-02-22 15:58:33 -0300

    refactor: refactoring middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -2,7 +2,18 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (!request.cookies.has('token')) {
+  if (!request.cookies.has('token') && request.nextUrl.pathname == '/') {
     return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if (
+    request.cookies.has('token') &&
+    (request.nextUrl.pathname == '/login' ||
+      request.nextUrl.pathname == '/register-user' ||
+      request.nextUrl.pathname == '/forgot-password' ||
+      request.nextUrl.pathname.startsWith('/password') ||
+      request.nextUrl.pathname.startsWith('/email'))
+  ) {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 }
