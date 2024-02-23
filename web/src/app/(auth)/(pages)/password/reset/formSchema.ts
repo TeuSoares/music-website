5e8560bf693a97d6fb61c03a0b50cea9dae5f882
TeuSoares <mateussoares114@gmail.com>
2024-02-23 14:30:42 -0300

    feat: add formSchema

--- web/src/app/(auth)/(pages)/password/reset/formSchema.ts
+++ web/src/app/(auth)/(pages)/password/reset/formSchema.ts
@@ -2,7 +2,6 @@ import { z } from 'zod'
 
 export const formSchema = z
   .object({
-    email: z.string().email(),
     password: z
       .string()
       .min(6, 'Password must contain at least 6 character(s)')
