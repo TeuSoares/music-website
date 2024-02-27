cf5efb7e36f3b10b110c1bc9f78f86f765da67a0
Mateus Soares <mateussoares114@gmail.com>
2024-02-27 06:20:45 -0300

    refactor: return boolean when try verify email

--- web/src/app/(protected)/email/EmailService.ts
+++ web/src/app/(protected)/email/EmailService.ts
@@ -1,25 +1,20 @@
-import { useRouter } from 'next/navigation'
-
-import { useError, useFetch, useAppContext } from '@/hooks'
+import { useFetch, useAppContext } from '@/hooks'
 
 export default function EmailService() {
-  const { setError } = useError()
   const { get } = useFetch()
   const { setIsLoading } = useAppContext()
-  const router = useRouter()
 
-  const handleVerifyEmail = async (url: string) => {
+  const handleVerifyEmail = async (url: string): Promise<boolean> => {
     setIsLoading(true)
 
     try {
       await get(url)
-    } catch (error: any) {
-      setError(error)
-
-      router.push('/')
+      setIsLoading(false)
+      return true
+    } catch (error) {
+      setIsLoading(false)
+      return false
     }
-
-    setIsLoading(false)
   }
 
   return { handleVerifyEmail }
