6f2db6c8c4657f05bd4f1137ce1cd66f99fcfb87
Mateus Soares <mateussoares114@gmail.com>
2024-02-29 05:57:29 -0300

    refactor: the field must be required

--- web/src/app/(auth)/(pages)/login/formSchema.ts
+++ web/src/app/(auth)/(pages)/login/formSchema.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod'
 
-export const formSchema = z
-  .object({
-    email: z.string().email(),
-    password: z.string(),
-  })
-  .required()
+export const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, 'Required'),
+})
 
 export type LoginFormData = z.infer<typeof formSchema>
