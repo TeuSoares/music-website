f1d9785f29bcff2858611882179a098bea73bc19
Mateus Soares <mateussoares114@gmail.com>
2024-03-06 06:26:02 -0300

    feat: add test to login

--- web/src/app/(auth)/(pages)/login/login.spec.tsx
+++ web/src/app/(auth)/(pages)/login/login.spec.tsx
@@ -1,6 +1,7 @@
+import LoginService from './LoginService'
 import Login from './page'
 
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 
 jest.mock('next/navigation', () => ({
   useRouter() {
@@ -9,6 +10,26 @@ jest.mock('next/navigation', () => ({
     }
   },
 }))
+
+// const mockNavigate = jest.fn()
+
+// jest.mock('next/navigation', () => ({
+//   ...jest.requireActual('next/navigation'),
+//   useRouter: () => {
+//     return {
+//       prefetch: () => null,
+//       mockNavigate,
+//     }
+//   },
+// }))
+
+jest.mock('./LoginService', () => {
+  const handleLogin = { handleLogin: jest.fn() }
+  return {
+    __esModule: true,
+    default: jest.fn(() => handleLogin),
+  }
+})
 
 describe('Login Page', () => {
   it('should render the login page correctly', () => {
@@ -29,5 +50,24 @@ describe('Login Page', () => {
     expect(passwordLabel).toBeInTheDocument()
     expect(createAccountLink).toBeInTheDocument()
     expect(forgotPasswordLink).toBeInTheDocument()
+  })
+
+  it('should be posible the user can login with correct credentials', async () => {
+    const { getByTestId } = render(<Login />)
+
+    const button = getByTestId('button-submit')
+
+    fireEvent.click(button)
+
+    const loginService = LoginService()
+
+    const obj = {}
+
+    loginService.handleLogin.call(obj, {
+      email: 'foo@example.com',
+      password: '12345678',
+    })
+
+    expect(1 + 1).toBe(2)
   })
 })
