0d28cca92c346c589d276939fcceba2ac098a7af
Mateus Soares <mateussoares114@gmail.com>
2024-04-29 06:25:27 -0300

    feat: add validateFile helper

--- web/src/utils/helpers.ts
+++ web/src/utils/helpers.ts
@@ -1,10 +1,28 @@
 export const builderQueryParams = (data: { [attr: string]: any }) =>
   new URLSearchParams(data).toString()
 
-export const setHours = (hours: number) => {
+export const setHours = (hours: number): Date => {
   const date = new Date()
   date.setHours(date.getHours() + hours)
   date.toLocaleDateString('pt-br')
 
   return date
+}
+
+export const validateFile = (path: string): boolean => {
+  const fileName = path.substring(path.lastIndexOf('\\') + 1)
+  const file = new File([path], fileName, { type: 'image/jpeg' })
+
+  if (!file || !file.name) {
+    return false
+  }
+
+  const allowedExtensions = ['jpg', 'jpeg', 'png']
+  const extension = file.name.split('.').pop()?.toLowerCase()
+
+  if (!extension || !allowedExtensions.includes(extension)) {
+    return false
+  }
+
+  return true
 }
