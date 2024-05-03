6049bcbd83cd63feee4fcf52f4938623abb275ce
Mateus Soares <mateussoares114@gmail.com>
2024-05-03 06:28:15 -0300

    refactor: Add support for config

--- web/src/hooks/useFetch.ts
+++ web/src/hooks/useFetch.ts
@@ -2,28 +2,36 @@ import { api } from '@/services/config/api'
 
 import { useError } from '.'
 
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export const useFetch = () => {
   const { setError } = useError()
 
-  const get = async (url: string) => {
-    const { data } = await api.get(url)
+  const get = async (url: string, config?: AxiosRequestConfig) => {
+    const { data } = await api.get(url, config)
     return data
   }
 
-  const post = async (url: string, formData?: object) => {
-    const { data } = await api.post(url, formData)
+  const post = async (
+    url: string,
+    formData?: object,
+    config?: AxiosRequestConfig,
+  ) => {
+    const { data } = await api.post(url, formData, config)
     return data
   }
 
-  const put = async (url: string, formData: object) => {
-    const { data } = await api.put(url, formData)
+  const put = async (
+    url: string,
+    formData: object,
+    config?: AxiosRequestConfig,
+  ) => {
+    const { data } = await api.put(url, formData, config)
     return data
   }
 
-  const destroy = async (url: string) => {
-    const { data } = await api.delete(url)
+  const destroy = async (url: string, config?: AxiosRequestConfig) => {
+    const { data } = await api.delete(url, config)
     return data
   }
 
